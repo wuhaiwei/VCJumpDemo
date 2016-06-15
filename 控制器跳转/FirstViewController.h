58838a30472f007e5689fddf809ded0a7b074228
wuhaiwei <wuhaiwei@kuaiyoujia.com>
2016-06-15 13:09:17 +0800

    Initial Commit

---
+++ 控制器跳转/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  控制器跳转
+//
+//  Created by admin on 16/6/15.
+//  Copyright © 2016年 KuaiYouJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ 控制器跳转/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  控制器跳转
+//
+//  Created by admin on 16/6/15.
+//  Copyright © 2016年 KuaiYouJia. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
